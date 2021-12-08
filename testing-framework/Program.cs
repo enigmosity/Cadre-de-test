3db3dc6d34789e37eb9ce478512f7da536c40e4d
Matt Simpson <matty.r.simpson@gmail.com>
2021-12-08 20:59:01 +1300

    First test
    pulled directly from TDD;by example - just to get me started. The initial test is pretty mindbending to write

---
+++ testing-framework/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var test = new WasRun("TestMethod");
+Console.WriteLine(test.wasRun);
+test.TestMethod();
+Console.WriteLine(test.wasRun);
+
+public class WasRun
+{
+    public bool wasRun;
+
+    public WasRun(string testMethod)
+    {
+        wasRun = false;
+    }
+
+    public void TestMethod()
+    {
+        wasRun = true;
+    }
+}
+
